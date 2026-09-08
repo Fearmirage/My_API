1c331cd9059873d0b094c9f90fcc4a2c5415874b
Fearmirage <giacomobalugani@gmail.com>
2026-09-08 14:44:15 +0200

    fix: fix app.module.ts

--- games-api/src/app.module.ts
+++ games-api/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller.js';
-import { AppService } from './app.service.js';
-import { SalesModule } from './sales/sales.module.js';
+import { SalesController } from './sales/sales.controller.js';
+import { SalesService } from './sales/sales.service.js';
+import { SalesModule } from './sales/sales.module.js'; //.ts becomes .js locally when ran
 
 @Module({
   imports: [SalesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [SalesController],
+  providers: [SalesService],
 })
 export class AppModule {}
