2767d98e0e3f0a6c0a5d81966783086b97c79705
Fearmirage <giacomobalugani@gmail.com>
2026-09-09 14:57:40 +0200

    feat: add pagination

--- games-api/src/sales/sales.controller.ts
+++ games-api/src/sales/sales.controller.ts
@@ -2,6 +2,7 @@ import {
     Controller,
     Get,
     Param,
+    Query,
 } from '@nestjs/common';
 
 import { SalesService } from './sales.service.js'; //.ts becomes .js locally when ran
@@ -13,8 +14,11 @@ export class SalesController {
     ) {}
 
     @Get()
-    findAll() {
-        return this.salesService.findAll();
+    findAll(@Query('page') page?: string, @Query('limit') limit?: string) { // Here, ? means the parameter is optional
+        return this.salesService.findAll(
+            page ? Number(page): 1, // Here, ? here is a compact if/else
+            limit ? Number(limit): 20 // if limit exists/is truthy, use Number(limit). Else, use 20
+        );
     }
 
     @Get(':id')
--- games-api/src/sales/sales.service.ts
+++ games-api/src/sales/sales.service.ts
@@ -20,8 +20,31 @@ export class SalesService {
         });
     }
 
-    findAll() {
-        return this.sales;
+    findAll(page: number = 1,limit: number = 20) {
+        limit = Math.min(limit,20) //max 20 elements per page
+        const total = this.sales.length;
+        const total_pages = Math.ceil(total/limit);
+        //Math.ceil(number) returns smallest integer greater than or equal to given number
+        //Prevent invalid page numbers
+        if (page < 1){
+            page = 1
+        } else if (page > total_pages) {
+            page = total_pages
+        };
+        const start = (page - 1) * limit;
+        const end = start + limit;
+
+        const data = this.sales.slice(start, end);
+
+        return {
+            data,
+            pagination: {
+                page,
+                limit,
+                total,
+                total_pages
+            }
+        };
     }
 
     findOne(rank: number) {
