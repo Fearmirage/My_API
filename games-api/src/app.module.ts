3790bb33291010292af8115b5c8deb1a12fcc042
Fearmirage <giacomobalugani@gmail.com>
2026-09-11 10:03:28 +0200

    feat: simplify with sales.module

--- games-api/src/app.module.ts
+++ games-api/src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { SalesController } from './sales/sales.controller.js';
-import { SalesService } from './sales/sales.service.js';//.ts becomes .js locally when ran
 
-@Module({
-  controllers: [SalesController],
-  providers: [SalesService],
-})
+import { SalesModule } from './sales/sales.module.js';
+
+@Module({ imports: [ SalesModule ], })
+
 export class AppModule {}
