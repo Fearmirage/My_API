38559c9ee440b7e18f2f06c10ba330cf3f2120cb
Fearmirage <giacomobalugani@gmail.com>
2026-09-16 11:05:13 +0200

    feat: add basic Swagger Documentation

--- games-api/src/main.ts
+++ games-api/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Video Games API')
+    .setDescription('API for managing video game sales data')
+    .setVersion('1.0') // just a display: needs to be changed with each version
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config); //Allows Swagger to read NestJS's structure
+  SwaggerModule.setup('api', app, document);
+  //
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
